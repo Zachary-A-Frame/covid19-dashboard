9a9b5dfe476fd2f93b018fd8ba05c79aad1b17c2
Zach <Zacaframe@gmail.com>
2020-09-19 14:09:57 -0400

    Added API Request Functionality

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 const express = require("express");
 const app = express();
 
+let data = require("./public/utility/csvjson.json");
+
 // use the express-static middleware
 app.use(express.static("public"));
 
+app.get("/data", function (req, res) {
+  res.header("Content-Type", 'application/json')
+  res.send(JSON.stringify(data))
+});
 // define the first route
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>");
